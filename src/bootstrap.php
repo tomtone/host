e0c24f84130386c1f0a1398f2d69959e4a7bc67f
Thomas von Gostomski <tgostomski@laptopc4125.intern.neusta.de>
2017-02-10 14:33:41 +0100

    renamed version in application

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,7 +3,7 @@
 require dirname(__DIR__) . "/vendor/autoload.php";
 use Neusta\Hosts\Console\Application;
 
-$app = new Application('Hosts', '1.0.0');
+$app = new Application('Hosts', '1.0.0-stable');
 $app->add(new \Neusta\Hosts\Command\ConnectCommand());
 $app->add(new \Neusta\Hosts\Command\ListCommand());
 $app->add(new \Neusta\Hosts\Command\AddCommand());
