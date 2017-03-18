2975d7091e5c19f4cdeb0ef0726cfd3baf2c91ad
Thomas von Gostomski <info@von-gostomski.eu>
2017-03-18 22:40:46 +0100

    added docs

---
+++ docs/_sources/installation.rst.txt
@@ -0,0 +1,9 @@
+Installation
+===============
+
+The recommended way to install Host as Phar with the ready to use installer.
+
+.. code-block:: bash
+
+    # Install Composer
+    curl -LSs https://box-project.github.io/box2/installer.php | php
---
+++ docs/searchindex.js
@@ -0,0 +1,9 @@
+/*
+ * This file is part of the teamneusta/hosts project.
+ * Copyright (c) 2017 neusta GmbH | Ein team neusta Unternehmen
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ * @license http://www.opensource.org/licenses/mit-license.html  MIT License
+ *
+ */
+
+Search.setIndex({docnames:["basic-usage","index","installation"],envversion:50,filenames:["basic-usage.rst","index.rst","installation.rst"],objects:{},objnames:{},objtypes:{},terms:{"short":1,"super":[],That:[],The:[1,2],about:[],address:1,allow:[],amount:1,automat:[],back:[],bar:[],basic:1,bold:[],box2:2,box:2,can:[],code:[],command:[],compos:2,configur:1,connect:1,content:1,curl:2,declar:[],defin:[],depend:[],emphasi:[],foo:[],getcompos:[],github:2,give:[],guzzl:[],has:[],have:[],helper:1,hierarchi:1,host:2,http:2,inspir:1,instal:1,ital:[],item:[],itself:[],just:[],least:[],length:[],like:[],list:[],littl:1,look:1,lss:2,main:[],maintain:1,manag:1,maxdepth:[],need:[],number:[],org:[],pain:1,paragraph:[],phar:2,php:2,port:1,power:[],project:[1,2],quot:[],readi:2,recommend:2,requir:[],rst:[],same:1,sampl:[],scope:1,set:[],sever:1,share:1,ssh:1,string:1,sudo:[],talk:[],them:[],time:1,titl:[],toctre:[],tool:[],unnumb:[],usag:1,use:2,user:1,wai:2,when:[],word:[],you:[],your:[]},titles:["Basic Usage","What is Host","Installation"],titleterms:{basic:0,host:1,inlin:[],instal:2,markup:[],subject:[],subtitl:[],thi:[],titl:[],usag:0,what:1}})
