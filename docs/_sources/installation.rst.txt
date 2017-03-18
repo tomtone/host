be08ad0716a2593fa8cd3cc3ade490c7ca786d6b
Thomas von Gostomski <info@von-gostomski.eu>
2017-03-18 22:55:49 +0100

    added docs

--- docs/_sources/installation.rst.txt
+++
@@ -1,9 +0,0 @@
-Installation
-===============
-
-The recommended way to install Host as Phar with the ready to use installer.
-
-.. code-block:: bash
-
-    # Install Composer
-    curl -LSs https://box-project.github.io/box2/installer.php | php
--- docs/searchindex.js
+++
@@ -1,9 +0,0 @@
-/*
- * This file is part of the teamneusta/hosts project.
- * Copyright (c) 2017 neusta GmbH | Ein team neusta Unternehmen
- * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
- * @license http://www.opensource.org/licenses/mit-license.html  MIT License
- *
- */
-
-Search.setIndex({docnames:["basic-usage","index","installation"],envversion:50,filenames:["basic-usage.rst","index.rst","installation.rst"],objects:{},objnames:{},objtypes:{},terms:{"short":1,The:[1,2],address:1,amount:1,basic:1,box2:2,box:2,compos:2,configur:1,connect:1,content:1,curl:2,github:2,helper:1,hierarchi:1,host:2,http:2,inspir:1,instal:1,littl:1,look:1,lss:2,maintain:1,manag:1,pain:1,phar:2,php:2,port:1,project:[1,2],readi:2,recommend:2,same:1,scope:1,sever:1,share:1,ssh:1,string:1,time:1,usag:1,use:2,user:1,wai:2},titles:["Basic Usage","What is Host","Installation"],titleterms:{basic:0,host:1,instal:2,usag:0,what:1}})
