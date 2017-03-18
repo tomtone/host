9096691380b20e8e2e72f0ff425bddc35d98aba9
Thomas von Gostomski <info@von-gostomski.eu>
2017-03-18 18:59:14 +0100

    testing with auto update

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,7 +3,7 @@
 require dirname(__DIR__) . "/vendor/autoload.php";
 use Neusta\Hosts\Console\Application;
 
-$app = new Application('Hosts', '1.5.0');
+$app = new Application('Hosts', '@package_version@');
 $app->add(new \Neusta\Hosts\Command\ConnectCommand());
 $app->add(new \Neusta\Hosts\Command\ListCommand());
 $app->add(new \Neusta\Hosts\Command\AddCommand());
